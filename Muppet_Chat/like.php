34cce7aa53a3c50ae01f7754c77e49e53e031a2f
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2023-12-07 16:04:33 +0000

    Likes fully works!

--- Muppet_Chat/like.php
+++ Muppet_Chat/like.php
@@ -37,31 +37,37 @@ if (isset($_POST['post_id'], $_POST['user_id'], $_POST['action'])) {
         $likes = $row['likes'];
         $dislikes = $row['dislikes'];
         $row_exists = 1;
-    } else {
-        $row_exists = 0;
-    }
-
-    // Handle like or dislike based on the action
-    if ($action == 'like') {
-        if ($likes == 1) {
-            $likes = 0;
-        } else {
-            $likes = 1;
-            if ($dislikes == 1) {
-                $dislikes = 0;
-            }
-        }
-    } elseif ($action == 'dislike') {
-        if ($dislikes == 1) {
-            $dislikes = 0;
-        } else {
-            $dislikes = 1;
+        // Handle like or dislike based on the action
+        if ($action == 'like') {
             if ($likes == 1) {
                 $likes = 0;
+            } else {
+                $likes = 1;
+                if ($dislikes == 1) {
+                    $dislikes = 0;
+                }
+            }
+        } elseif ($action == 'dislike') {
+            if ($dislikes == 1) {
+                $dislikes = 0;
+            } else {
+                $dislikes = 1;
+                if ($likes == 1) {
+                    $likes = 0;
+                }
             }
         }
+    } else {
+        $row_exists = 0;
+        if ($action == 'like') {
+            $likes = 1;
+            $dislikes = 0;
+        } else {
+            $likes = 0;
+            $dislikes = 1;
+        }
+        
     }
-
     // Update or insert the like in the database
     if ($row_exists == 1) {
         $query = "UPDATE `likes` SET likes = $likes, dislikes = $dislikes WHERE user_id = $user_id AND post_id = $post_id";
